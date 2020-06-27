b90384a70f44d4859d90cdba6bcf2e50e3e5a713
Swapnil Banga <swapnil.galaxy@gmail.com>
2020-06-28 01:00:10 +0530

    ADD - homepage component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css';
-import HomePage from './homepage.component';
+import React from 'react'
+import './App.css'
+import HomePage from './pages/homepage/homepage.component'
 
 function App() {
-  return (
-    <div className="App">
-      <HomePage />
-    </div>
-  );
+    return (
+        <div className="App">
+            <HomePage />
+        </div>
+    )
 }
 
-export default App;
+export default App
